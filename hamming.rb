0c4773adf45ccae98aab9a4e96f3655eac759704
Stephen Weigel <stephen@stephenweigel.com>
2014-06-05 11:26:47 -0400

    First Refactor

--- hamming.rb
+++ hamming.rb
@@ -1,30 +1,27 @@
 class Hamming
 
-  def initialize
-  end
-
   def self.compute(strand_one, strand_two)
     new.compute(strand_one, strand_two)
   end
 
   def compute(strand_one, strand_two)
+    # set initial distance to 0
+    # assuming no mutations
     distance = 0
+    
     # Convert strands to char arrays
-    first_strand = strand_one.chars
-    second_strand = strand_two.chars
-
     # zip the two arrays for easy comparison
-    comparison_array = first_strand.zip(second_strand)
+    comparison_array = strand_one.chars.zip(strand_two.chars)
 
     # check for differences
-    # @distance += 1 for each difference
+    # distance += 1 for each difference
     comparison_array.each do |i|
       next if i[0] === i[1]
       next if i[0].nil? || i[1].nil?
       distance +=1
     end
 
-    # return the current distance
+    # return the computed distance
     distance
   end
 end
